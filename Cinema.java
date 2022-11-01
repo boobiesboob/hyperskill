19dd746ace28cc1f5a803d576baba4caa40a83c2
Mo$he <e.bubnov@gmx.de>
2022-11-01 15:55:20 +0100

    File to repo

---
+++ Cinema.java
@@ -0,0 +1,14 @@
+public class Cinema {
+    /**
+     * first stage
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Cinema:");
+        System.out.println("  1 2 3 4 5 6 7 8");
+        for (int i = 1; i < 8; i++) {
+            System.out.print(i + " ");
+            System.out.println("S S S S S S S S");
+        }
+    }
+}
